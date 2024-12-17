e3afebaee5528e5c60ad0a50f581fac5ba585b81
Douglas Ricardo <douglas4991@hotmail.com>
2024-12-17 18:50:30 -0300

    Firs Steps in Java with Spring Boot 4

---
+++ FirtsStepsinJavaSpringboot/Rest-with-Spring-boot4-and-java-erudio/src/main/java/br/com/erudio/RestWithSpringBoot4AndJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBoot4AndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBoot4AndJavaErudioApplication.class, args);
+	}
+
+}
